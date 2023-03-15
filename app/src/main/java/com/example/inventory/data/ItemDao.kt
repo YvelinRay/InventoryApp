eae2f9427a4ea680fabc0f53504e142cf73f46a6
YvelinRay <42838665+YvelinRay@users.noreply.github.com>
2023-03-15 10:35:36 +0100

    Commit final

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,24 @@
+/* Auteur : Yvelin A. Ray
+* Date : 15.03.2023 */
+package com.example.inventory.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+    @Query("SELECT * from item ORDER BY name ASC")
+    fun getItems(): Flow<List<Item>>
+
+    @Query("SELECT * from item WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+}
